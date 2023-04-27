f542b4be491efeb81d26afdff14844bfd1eb913f
Arthurim <arthurbdauphine@gmail.com>
2023-04-27 09:36:31 +0100

    feat: list of jks
    Signed-off-by: Arthurim <arthurbdauphine@gmail.com>

--- scripts/check_krisha_dev.bash
+++ scripts/check_krisha_dev.bash
@@ -6,6 +6,6 @@ alias py=$a
 py -m launch.kz.check_krisha Astana Headliner 1 DEV;
 #py -m launch.kz.check_krisha Astana Aqua 1 DEV;
 #py -m launch.kz.check_krisha Astana nexpo 1 DEV;
-#py -m launch.kz.check_krisha Almaty Jazz 2 DEV;
+py -m launch.kz.check_krisha Almaty Jazz 2 DEV;
 #py -m launch.kz.check_krisha Astana Flora 1 DEV;
 #py -m launch.kz.check_krisha Astana Tokyo 1 DEV;
