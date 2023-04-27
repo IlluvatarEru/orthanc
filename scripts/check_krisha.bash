9ee7fa94600906b7a67865f46c3de44cfd5c4e74
Arthurim <arthurbdauphine@gmail.com>
2023-04-27 19:49:56 +0100

    fix: headliner 1bed
    Signed-off-by: Arthurim <arthurbdauphine@gmail.com>

--- scripts/check_krisha.bash
+++ scripts/check_krisha.bash
@@ -3,7 +3,7 @@ shopt -s expand_aliases
 . scripts/utils.bash
 a=$(get_python_alias)
 alias py=$a
-py -m launch.kz.check_krisha Astana Headliner 2 PROD;
+py -m launch.kz.check_krisha Astana Headliner 1 PROD;
 #py -m launch.kz.check_krisha Astana Aqua 1 PROD;
 #py -m launch.kz.check_krisha Astana nexpo 1 PROD;
 py -m launch.kz.check_krisha Almaty Jazz 2 PROD;
