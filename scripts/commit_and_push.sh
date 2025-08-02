8dad2e625fcd0094c3bcd9b323373e6f9bc1c8c1
Arthurim <arthurbdauphine@gmail.com>
2025-08-02 18:41:13 +0200

    feat: new app
    Signed-off-by: Arthurim <arthurbdauphine@gmail.com>

--- scripts/commit_and_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#  commits and push
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-bash scripts/commit.sh
-git push --set-upstream origin $branch
--- src/utils/formatting.py
+++
@@ -1,6 +0,0 @@
-def format_price_to_million_tenge(price):
-    return str(round(price / 1e6, 2)) + 'M₸'
-
-
-def format_prices_to_million_tenge(prices):
-    return prices.apply(lambda x: format_price_to_million_tenge(x))
